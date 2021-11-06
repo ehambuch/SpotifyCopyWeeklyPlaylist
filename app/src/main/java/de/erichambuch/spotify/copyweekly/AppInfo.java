9d09812c42396b709dc549bd40c7aeb52fbb31cc
Eric Hambuch <erichambuch@googlemail.com>
2021-11-06 22:39:09 +0100

    Version 1.1

--- app/src/main/java/de/erichambuch/spotify/copyweekly/AppInfo.java
+++ app/src/main/java/de/erichambuch/spotify/copyweekly/AppInfo.java
@@ -9,7 +9,7 @@ package de.erichambuch.spotify.copyweekly;
  * V0.6 (6): Nach LocalBroadcast-Receiver deprecated: UI/Service-Kommunikation umgebaut, neue Spotify API!
  * v0.7 (7): fehlende Permission für Wake-Lock
  * v1.0 (8): Target Android 11
- * : Updated libs
+ * V1.1 (10): Updated Updated libs
  * Idee: Liste mit Checkmarks für Fortschritt
  */
 public class AppInfo {
